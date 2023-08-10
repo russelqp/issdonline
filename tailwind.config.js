8103e31973312b5fb920b6e724363b227b76bde0
russelqp <russelquiam@hotmail.com>
2023-08-10 18:00:06 -0500

    se termino de agregar el menu min y configuro responsive de header y quota inicial

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,15 @@ module.exports = {
     "./src/**/*.{html,js}",
     "./public/**/*.{html,js}",
     "./node_modules/flowbite/**/*.js",
-
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Gordita','sans-serif','ui-sans-serif', 'system-ui'],
+      // 'serif': ['ui-serif', 'Georgia'],
+      // 'mono': ['ui-monospace', 'SFMono-Regular'],
+      // 'display': ['Oswald'],
+      // 'body': ['"Open Sans"'],
+    },
     extend: {
       backgroundImage: {
         'hero-pattern': "url('/images/fondo_rayas_verdes.png')"
