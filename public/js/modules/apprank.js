082ea69af5340ed0e76f904280af271c4e480e52
russelqp <russelquiam@hotmail.com>
2024-03-21 23:28:33 -0500

    -Se agrego database realtime rankin omni

---
+++ public/js/modules/apprank.js
@@ -0,0 +1,12 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyCSJluXfg3wupCaQAW1n0M5r5eNcDzIzuw",
+    authDomain: "issdonline-20e25.firebaseapp.com",
+    databaseURL: "https://issdonline-20e25-default-rtdb.firebaseio.com",
+    projectId: "issdonline-20e25",
+    storageBucket: "issdonline-20e25.appspot.com",
+    messagingSenderId: "959101042524",
+    appId: "1:959101042524:web:2295bdb2d720bb5ff5cb99"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  export {firebaseConfig}
---
+++ public/js/ranking.js
@@ -0,0 +1,42 @@
+import {firebaseConfig} from './modules/apprank.js';
+document.createElement('h1').innerHTML="Ranking Omni";
+
+//obtener datos
+function retrieveInfos(){
+    let ref = firebase.database().ref("omniranking");
+    ref.on("value", gotData);
+}
+
+function gotData(data){
+    
+    let info = data.val()
+    createHTML(info)
+}
+
+function createHTML(datos){
+    
+    let elementos = datos;
+    let mainhtml = document.querySelector('#portada')
+    console.log(elementos)
+    //creamos un nuevo array con los elementos de acuerdo a los filtros
+    var main_ranking = "";
+    function showHtml(){
+        main_ranking+= `<ul class="listado_rank">`;
+        main_ranking+= `<li class="li_headers lg:w-full grid grid-cols-4 justify-items-center bg-slate-600">
+        <span>Position</span><span>Id</span><span>Name</span><span>Score</span>      
+        </li>`;
+        elementos.forEach(element => {
+            if(element.POS <=10){
+                main_ranking+= `<li class="li_item lg:w-full grid grid-cols-4 justify-items-center border-1 border-blue-50 odd:bg-green-300 even:bg-green-400"><span>${element.POS}</span> <span>${element.ID}</span> <span>${element.NOME}</span> <span>${element.SCORE}</span>`;
+            } else{
+                main_ranking+= `<li class="li_item lg:w-full grid grid-cols-4 justify-items-center odd:bg-gray-200  even:bg-gray-300 border-1 border-blue-50 bg-blue-300"> <span>${element.POS}</span> <span>${element.ID}</span> <span>${element.NOME}</span> <span>${element.SCORE}</span>`;
+            }
+            
+        }); 
+        main_ranking+= "</ul>";    
+        mainhtml.insertAdjacentHTML('afterend', main_ranking);
+    } 
+    showHtml()
+   
+}
+retrieveInfos()
