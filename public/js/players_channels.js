8146fdc00c3b3375290bb2bc35d604e195e2a467
russelqp <russelquiam@hotmail.com>
2024-07-28 16:24:06 -0500

    funcionando carja json con iconos de url players

--- public/js/players_channels.js
+++ public/js/players_channels.js
@@ -14,30 +14,54 @@
         let youtube_icon = "youtube-icon";
         let twitch_icon = "twitch-icon";
 
+        //- Clases iconos
+        let svg_icon_facebook2 = `<svg class="icon icon-facebook2"><use xlink:href="#icon-facebook2"></use></svg>`;
+        let svg_icon_youtube = `<svg class="icon icon-youtube"><use xlink:href="#icon-youtube"></use></svg>`;
+        let svg_icon_twitch = `<svg class="icon icon-twitch"><use xlink:href="#icon-twitch"></use></svg>`;
         var players = jsonObj['channels_player'];
         //-<a href='${players[i].redes_player}' title='${players[i].nick_player}' class='${a_icon(players[i].redes_player)}'>${players[i].nick_player}
         function a_icon(redes){
-            var li_player = document.querySelector('.li_player');
-            var clase_icon = document.createElement('div')
-            clase_icon.setAttribute()
-           
+        
+            var li_player = document.querySelector('.id-0'+i);
+            let __iconos = document.querySelector('.__iconos')
+            var clase_icon = document.createElement('div');
+            
+
             for(var e = 0; e<redes.length; e++){
                 if (redes[e].includes('facebook')){
-                   facebook_icon;
+                    var a_ico_f = document.createElement('a')
+                    a_ico_f.setAttribute('href', redes[e]);
+                    a_ico_f.setAttribute('title', "Facebook Channel");
+                    a_ico_f.classList.add('icon-facebook');
+                    a_ico_f.innerHTML = svg_icon_facebook2;
+                    clase_icon.appendChild(a_ico_f);
                 }
                 if (redes[e].includes('youtube')){
-                    youtube_icon
+                    var a_ico_y = document.createElement('a')
+                    a_ico_y.setAttribute("href", redes[e]);
+                    a_ico_y.setAttribute('title', "Youtube Channel");
+                    a_ico_y.classList.add('icon-youtube');
+                    a_ico_y.innerHTML = svg_icon_youtube;
+                    clase_icon.appendChild(a_ico_y);
+                   
                 }
                 if (redes[e].includes('twitch')){
-                    twitch_icon
+                    var a_ico_t = document.createElement('a')
+                    a_ico_t.setAttribute("href", redes[e]);
+                    a_ico_t.setAttribute('title', "Twitch Channel");
+                    a_ico_t.classList.add('icon-twitch');
+                    a_ico_t.innerHTML = svg_icon_twitch;
+                    clase_icon.appendChild(a_ico_t);
+                   
                 }
+                console.log("se encontraron "+redes.length+" redes")
+                
             }
+            return clase_icon.outerHTML;
         }
         for(var i = 0; i < players.length; i++){
-            container_li += `<li><div class="__container--info"><h2> Hola, yo soy <span>${players[i].nick_player[0]}</span></h2><h3>${players[i].presentacion_player}</h3><p>${players[i].bio_player}</p><div class="__iconos></div></div><div class="__container--img"><figure><img src='${players[i].img_player}' width="100" height="130" alt='avatar_jugador_issd${players[i].nick_player}'/></figure></div></li>`;
+            container_li += `<li class=${"id-0"+i}><div class="__container--info"><h2> Hola, yo soy <span>${players[i].nick_player[0]}</span></h2><h3>${players[i].presentacion_player}</h3><p>${players[i].bio_player}</p><div class="__iconos">${a_icon(players[i].redes_player)}</div></div><div class="__container--img"><figure><img src='${players[i].img_player}' width="100" height="130" alt='avatar_jugador_issd${players[i].nick_player}'/></figure></div></li>`;
         }
         ul_channels.innerHTML = container_li;
-        console.log(ul_channels)
         //ul_channels.innerHTML= container_li.outerHTML;
-
     }
